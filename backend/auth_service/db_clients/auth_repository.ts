0f3d92b7736e99b106e432e8d5c3dff8a276895f
RadoslavCvetanov <radoslav.a.tsvetanov.2021@elsys-bg.org>
2024-03-14 01:32:18 +0200

    made basic structure for the workouts service

---
+++ backend/auth_service/db_clients/auth_repository.ts
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose, { Document, Schema } from 'mongoose';
+
+const uri: string = process.env.MONGO_URI || '';
+mongoose.connect(uri);
+
+interface IUser extends Document {
+  username: string;
+  password: string;
+  has_2fa: boolean;
+}
+
+const userSchema: Schema = new mongoose.Schema({
+  username: String,
+  password: String,
+  has_2fa: Boolean,
+});
+
+const User = mongoose.model('User', userSchema);
+
+export interface IDB {
+  get_user(username: string): Promise<IUser | null>;
+  create_user(
+    username: string,
+    password: string,
+  ): Promise<{ success: boolean; message?: string }>;
+}
+
+class DB implements IDB {
+  constructor() {}
+
+  async get_user(username: string): Promise<IUser | null> {
+    try {
+      const user: IUser | null = await User.findOne({ username });
+      return user; // Return true if the user is found, otherwise false
+    } catch (error) {
+      console.error('Error checking for user:', error);
+      return null;
+    }
+  }
+
+  async create_user(
+    username: string,
+    password: string,
+  ): Promise<{ success: boolean; message?: string }> {
+    try {
+      console.log(username, password);
+      const existingUser: IUser | null = await User.findOne({ username });
+      console.log(existingUser);
+      if (existingUser) {
+        return { success: false, message: 'Username already exists' };
+      }
+
+      const newUser = new User({ username, password, has_2fa: false });
+      await newUser.save();
+
+      return { success: true, message: 'User created successfully' };
+    } catch (error) {
+      console.error('Error creating user:', error);
+      return { success: false, message: 'Error creating user' };
+    }
+  }
+}
+
+export { DB };
---
+++ backend/workouts_service/README.md
@@ -0,0 +1,93 @@
+# Authentication Service
+
+This is a simple authentication service built with Node.js, Express, bcrypt, and JSON Web Tokens (JWT).
+
+## Features
+
+- User signup: Allows users to register by providing a username and password.
+- User login: Validates user credentials and issues a JWT token upon successful authentication.
+
+## Setup
+
+```bash
+cd authentication-service
+npm install
+```
+
+enter env as shown in
+
+```bash
+.env.example
+```
+
+## Start the server:
+
+```bash
+
+npm start
+
+The server will start running on port 3000 by default.
+Endpoints
+POST /auth/signup
+
+Registers a new user with the provided username and password.
+
+Request body:
+
+json
+
+{
+  "username": "your_username",
+  "password": "your_password"
+}
+
+Response:
+
+    201 Created: User created successfully.
+    400 Bad Request: If the username is already taken or other validation errors occur.
+    500 Internal Server Error: If a server error occurs.
+
+POST /auth/login
+
+Logs in a user with the provided username and password.
+
+Request body:
+
+json
+
+{
+  "username": "your_username",
+  "password": "your_password"
+}
+
+Response:
+
+    200 OK: If the login is successful, returns a JWT token.
+    401 Unauthorized: If the username or password is incorrect.
+    404 Not Found: If the user does not exist.
+    500 Internal Server Error: If a server error occurs.
+
+POST /testing_route
+
+Testing route to verify a JWT token.
+
+Request body:
+
+json
+
+{
+  "token": "your_jwt_token"
+}
+
+Response:
+
+    201 Created: If the token is valid, returns the decoded token payload.
+    400 Bad Request: If the token is invalid or expired.
+
+Dependencies
+
+    express: "^4.17.1"
+    bcrypt: "^5.0.1"
+    jsonwebtoken: "^8.5.1"
+    body-parser: "^1.19.0"
+```
