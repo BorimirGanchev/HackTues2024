5eaa9ba0d8f4d816828cc70f687877ce835d1134
Flychuban <kaloanas07@gmail.com>
2024-03-14 18:15:09 +0200

    add low_pass filter

--- ML/src/models/data_preprocess.py
+++ ML/src/models/data_preprocess.py
@@ -2,7 +2,13 @@ import pandas as pd
 import numpy as np
 import math
 import scipy
-from sklearn.neighbors import LocalOutlierFactor
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from features.DataTransformation import LowPassFilter, PrincipalComponentAnalysis
+from features.TemporalAbstraction import NumericalAbstraction
+from features.FrequencyAbstraction import FourierTransformation
+from sklearn.cluster import KMeans
 
 
 def remove_ouliers(df):  
@@ -64,6 +70,67 @@ def remove_ouliers(df):
             n_outliers = len(dataset) - len(dataset[col].dropna())
             print(f"Removed {n_outliers} outliers for {label} - {col}")
     return outlier_removed_df
+
+def build_features(df):
+    predictor_columns = list(df.columns[:6])
+    
+    def interpolation(df):
+        # --------------------------------------------------------------
+        # Dealing with missing values (imputation)
+        # --------------------------------------------------------------
+        for col in predictor_columns:
+            df[col] = df[col].interpolate()
+        
+    # --------------------------------------------------------------
+    # Butterworth lowpass filter
+    # --------------------------------------------------------------
+    def butterworth_lowpass_filter(df):
+
+        df_lowpass = df.copy()
+        LowPass = LowPassFilter()
+
+        fs = 1000 / 200
+        cutoff = 1.3
+        df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5) 
+
+        for col in predictor_columns:
+            df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+            df_lowpass[col] = df_lowpass[col + "_lowpass"]
+        del df_lowpass[col + "_lowpass"]
+
+        return df_lowpass
+    
+    def apply_pca(df):
+        # --------------------------------------------------------------
+        # Principal component analysis PCA
+        # --------------------------------------------------------------
+        df_pca = df.copy()
+        PCA = PrincipalComponentAnalysis()
+
+        pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_columns)
+
+        df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
+        
+        return df_pca
+    
+    def sum_squares(df):
+        # --------------------------------------------------------------
+        # Sum of squares attributes
+        # --------------------------------------------------------------
+        df_squared = df_pca.copy()
+        acc_r = df_squared['acc_x'] ** 2 + df_squared['acc_y'] ** 2 + df_squared['acc_z'] ** 2
+        gyr_r = df_squared['gyr_x'] ** 2 + df_squared['gyr_y'] ** 2 + df_squared['gyr_z'] ** 2
+
+        df_squared['acc_r'] = np.sqrt(acc_r)
+        df_squared['gyr_r'] = np.sqrt(gyr_r)
+        
+        return df_squared
+        
+        
+    interpolation(df)
+    df_lowpass = butterworth_lowpass_filter(df)
+    df_pca = apply_pca(df_lowpass)
+    df_squared = sum_squares(df_pca)
 
 df = pd.read_pickle("../../data/interim/01_data_processed.pkl") # Load data
 outlier_removed_df = remove_ouliers(df) # Remove outliers
