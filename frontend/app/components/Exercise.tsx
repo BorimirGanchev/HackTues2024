fa6c545acb00175d203c05d4197fb175877b4d8c
vzlat <vzlat@VIKI01>
2024-03-15 15:39:45 +0200

    exercise_changes

--- frontend/app/components/Exercise.tsx
+++ frontend/app/components/Exercise.tsx
@@ -1,5 +1,6 @@
+// ExerciseComponent.tsx
 import React, { useState } from 'react';
-import { Exercise } from '../types/ExerciseServiceTypes';
+import { Exercise } from '../types/ExerciseServiceTypes'; // Assuming this is where your type is correctly defined
 
 export const ExerciseComponent: React.FC<{exercise: Exercise}> = ({exercise}) => {
   // State to hold the weight input
@@ -14,21 +15,21 @@ export const ExerciseComponent: React.FC<{exercise: Exercise}> = ({exercise}) =>
     <div>
       <h3>{exercise.name}</h3>
       <h3>{exercise.reps}</h3>
-      {exercise.weight ? (
+      {exercise.weight !== undefined ? ( // Assuming weight can be 0, which should be truthy in this context
         <div>{exercise.weight}</div>
       ) : (
-        
-        <>        
+        <form> {/* Form tag added for completeness */}
           <label htmlFor="weight-input">Weight: </label>
           <input
             id="weight-input"
             type="number" 
             value={weight}
             onChange={handleWeightChange}
-          
           />
-        </>
+        </form>
       )}
     </div>
   );
 };
+export { Exercise };
+
