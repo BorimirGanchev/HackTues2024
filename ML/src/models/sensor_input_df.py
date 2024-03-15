19d98cb706e306a6f64bf8625b09cb48db0f40c9
Flychuban <kaloanas07@gmail.com>
2024-03-15 13:20:16 +0200

    add column for ms in sensor_df

--- ML/src/models/sensor_input_df.py
+++ ML/src/models/sensor_input_df.py
@@ -4,6 +4,7 @@ def sensor_data_to_df(sensor_data):
 
 
     df = pd.DataFrame({
+        'timestamp_ms': sensor_data['ms'],
         'acc_x': sensor_data['accel']['x'],
         'acc_y': sensor_data['accel']['y'],
         'acc_z': sensor_data['accel']['z'],
